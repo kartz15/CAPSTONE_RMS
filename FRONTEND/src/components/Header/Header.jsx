02044a72761323f8244e1f06bd0cdecb82443dc1
kartz15 <173812770+kartz15@users.noreply.github.com>
2024-10-08 01:17:45 -0400

    Latest

--- FRONTEND/src/components/Header/Header.jsx
+++ FRONTEND/src/components/Header/Header.jsx
@@ -11,11 +11,11 @@ const Header = ({ cartCount }) => {
                 </div>
                 <div className="nav_links">
                     <Link className="nav_link" id="nav_id" to="/menu">ORDER NOW</Link>
-                    <Link className="nav_link" to="/">Home</Link>
-                    <Link className="nav_link" to="/menu">Menu</Link>
-                    <Link className="nav_link" to="/login">Sign In</Link> 
-                    <Link className="nav_link" to="/cart">Cart ({cartCount})</Link> 
-                    <Link className='nav_link' to="/order-history">Orders</Link>
+                    <Link className="nav_link" to="/">              <i class="fa-solid fa-house"></i> Home</Link>
+                    <Link className="nav_link" to="/menu">          <i class="fa-solid fa-bars"></i> Menu</Link>
+                    <Link className="nav_link" to="/login">         <i class="fa-solid fa-right-to-bracket"></i> Sign In</Link> 
+                    <Link className="nav_link" to="/cart">          <i className="fas fa-shopping-cart"></i> Cart ({cartCount})</Link> 
+                    <Link className='nav_link' to="/order-history"> <i class="fa-solid fa-clock-rotate-left"></i> Orders</Link>
                 </div>
             </nav>
         </header>
